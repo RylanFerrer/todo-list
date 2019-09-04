85e937b03adc493f8c32cdf6c639f8fd58bb71f3
RylanFerrer <rylanferrer@gmail.com>
2019-09-03 21:53:55 -0700

    added mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -11,32 +12,71 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-  const workItems = []; // this array is to push tasks done on the work list
-  const items = []; // this array is to push tasks done on the normal webpage
-  app.get("/",function(req,res){
-  let day = date.getDate(); // let the exported function from date.js getDate() equal to the variable day
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-res.render("list", { // this renders the variables into the list.ejs template
-  listTitle: day,
-  newListItem: items
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todoList!"
 });
+
+const item2 = new Item({
+  name:"Hit the + button to add a new item."
+});
+
+const defaultItems = [item1,item2];
+
+
+
+
+  app.get("/",function(req,res){
+      let day = date.getDate(); // let the exported function from date.js getDate() equal to the variable day
+    Item.find({}, (err, foundItems) => {
+      if(foundItems.length === 0) {
+        Item.insertMany(defaultItems, (err) =>{
+          if(err){
+            console.log(err);
+          } else {
+            console.log("Successfully saved to default items to DB");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", { // this renders the variables into the list.ejs template
+          listTitle: day,
+          newListItem: foundItems
+        });
+      }
+    });
+});
+
+
 
 app.post("/", function(req,res){ // this posts things to the root
 
-  let item = req.body.newItem; // the item variable is equal to the tasks the user inputs into the search bar
-
-  if (req.body.list === "Work") { // if the lists is in the /work root then push it to the workItems array and redirect it to /work root
-    workItems.push(item);
-    res.redirect("/work");
-  }
-  else
-  {
-    items.push(item); // if it does not push it to the main page's list
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem; // the item variable is equal to the tasks the user inputs into the search bar
+  const item = new Item({
+    name:itemName
+  });
+  item.save();
+  res.redirect("/");
 
 });
 
+
+app.post("/delete", (req,res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully Deleted");
+    }
+  });
+  res.redirect("/");
 });
 
 app.get("/work", function(req,res){ //get the work root
